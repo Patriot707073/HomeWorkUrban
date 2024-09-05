a46240bde3285ade5911c7486d6a23820e46a752
Евгений <154864185+Patriot707073@users.noreply.github.com>
2024-09-05 10:38:41 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,23 @@
+# задание 2
+my_dict = {"Андрей":2000, "Боря":2005, "Юра":2007}
+print(my_dict)
+print(my_dict["Юра"])
+print(my_dict.get("Сергей","Нет такого"))
+my_dict.update({"Ара":2002,"Зульфия":2004})
+a= my_dict.pop("Ара")
+print (a)
+print(my_dict)
+
+# задание 3
+my_set = {1,"автомобиль",3,1,44,5,6,2,4,3,9.9}
+print(my_set)
+my_set.add(0)
+my_set.add(10)
+my_set.remove("автомобиль")
+print(my_set)
+
+
+
+
+
+
