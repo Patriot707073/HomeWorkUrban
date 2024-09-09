84f202eb4ad23fe278b153cd2d10efc1997ce31a
Евгений <154864185+Patriot707073@users.noreply.github.com>
2024-09-09 10:42:22 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,10 @@
+first = int(input("Введите целое число "))
+second = int(input("Введите целое число "))
+third  = int(input("Введите целое число "))
+if first==second==third:
+    print (3)
+elif first==second or first==third or third==second:
+    print (2)
+else:
+    print (0)
+
