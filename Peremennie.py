4bbc3c15a59aaacc745e508e5183c273c5c5ac2d
Евгений <154864185+Patriot707073@users.noreply.github.com>
2024-09-13 14:08:10 +0300

    Delete Peremennie.py

--- Peremennie.py
+++
@@ -1,7 +0,0 @@
-number_compeled = 12
-number_hours = 1.5
-name_cours = "Python"
-time_task = number_hours/number_compeled
-print (name_cours,",всего задач:",number_compeled,",затрачено часов:",number_hours,",среднее время выполнения",time_task,"часа")
-
-
