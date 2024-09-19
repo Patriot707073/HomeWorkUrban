0428b1ca002bf2c57f07927abeb1eee4455e06ab
Евгений <154864185+Patriot707073@users.noreply.github.com>
2024-09-19 07:59:05 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,17 +1,12 @@
-def send_email(message, recipient, sender="university.help@gmail.com"):
+def send_email(message, recipient, *,sender="university.help@gmail.com"):
     domens = [".com", ".ru", ".net"] # список доменов
 
     if "@" not in recipient or "@" not in sender:# проверка на наличие "@" в recipient и sender.
         return (f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
 
-    domain_valid = False
-    for i in domens:
-        if i in recipient and i in sender: # проверяю в переменных recipient и sender наличие доменов ".com", ".ru", ".net"
-            domain_valid = True
-            break
-
-    if not domain_valid:
-        return (f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}: Некорректный домен")
+    if not any(recipient.endswith(dmn) for dmn in domens) or not any(
+            sender.endswith(dmn) for dmn in domens):
+        return (f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
 
     if recipient.lower() == sender.lower():
         return ("Нельзя отправить письмо самому себе!")
@@ -26,3 +21,4 @@ print(send_email('Это сообщение для проверки связи',
 print(send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com'))
 print(send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk'))
 print(send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru'))
+
